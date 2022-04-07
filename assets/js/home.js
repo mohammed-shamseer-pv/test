836c524fdb4ab29268792d4bf83c875f3b881e7b
shamseer <mohammedshamseerp@gmail.com>
2022-04-06 17:28:43 -0700

    test

--- assets/js/home.js
+++ assets/js/home.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
       $('.navbar').toggleClass('nav-toggle');
     });
   
-    // $(window).on('load scroll',function(){
-    //   $('.fa-bars').removeClass('fa-times');
-    //   $('.navbar').removeClass('nav-toggle');
+    $(window).on('load scroll',function(){
+      $('.fa-bars').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
   
-    //   if($(window).scrollTop() > 30){
-    //     $('.header').css({'background':'#00931f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
-    //   }else{
-    //     $('.header').css({'background':'none','box-shadow':'none'});
-    //   }
-    // });
+      if($(window).scrollTop() > 30){
+        $('.header').css({'background':'#00931f','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+      }else{
+        $('.header').css({'background':'none','box-shadow':'none'});
+      }
+    });
   
   
     $('.accordion-header').click(function(){
